7e7ce90d93f105e73cdfa9c0de786852b29d5e3d
Cedric <crubbertire@rogers.com>
2025-09-24 14:26:33 -0400

    Create function to calculate tip

--- app.js
+++ app.js
@@ -117,14 +117,34 @@ Example: maxOfThree(17, 4, 9) should return 17.
 Complete the exercise in the space below:
 */
 
-const maxOfThree = (...nums) => {
+const maxOfNums = (...nums) => { // Using a rest parameter, we can expand this to catch any amount of nums provided, so that we don't need a unique function for every quantity!
   let topNum = 0;
-  for (num of nums) {
-    if (num > topNum) {
-      topNum = num;
+  for (num of nums) {  // Loops through the array of nums, declaring the num at every loop.
+    if (num > topNum) { // Check if the current num is greater than the previously logged largest num.
+      topNum = num; // Swaps it!
     }
   }
   return topNum;
 }
 
-console.log('Exercise 6 Result:', maxOfThree(5, 10, 8));
+console.log('Exercise 6 Result:', maxOfNums(5, 10, 8));
+
+
+
+/*
+Exercise 7: calculateTip()
+
+Create a function called calculateTip. It should take two arguments: 
+the bill amount and the tip percentage (as a whole number). 
+The function should return the amount of the tip.
+
+Example: calculateTip(50, 20) should return 10.
+
+Complete the exercise in the space below:
+*/
+
+const calculateTip = (bill, percentage) => {
+  return Math.trunc(bill * (percentage/100)); // Math.trunc to remove decimal points (forces int if float)
+}
+
+console.log('Exercise 7 Result:', calculateTip(50, 20));
